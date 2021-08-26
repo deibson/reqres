290106f90a669defb8083391fa83a9f448a02f58
Deibson Fagner <deibson.fagner@gmail.com>
2021-08-26 01:54:08 -0300

    init

---
+++ src/main/java/pojo/User.java
@@ -0,0 +1,30 @@
+package pojo;
+
+public class User {
+    private String name;
+    private String job;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "\"name\":\"" + name + "\"," +
+                "\"job\":\"" + job + "\"" +
+                "}";
+    }
+}
